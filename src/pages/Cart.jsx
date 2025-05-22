3ac34582e7da4611f379a91b39f86842d0cdc62a
agusbattista <130807422+agusbattista@users.noreply.github.com>
2025-05-22 01:04:00 -0300

    carrito basico con cantidad de cada libro, eliminar de a un libro, vaciar carrito y precio total

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,34 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import RenderCartCard from "../components/RenderCartCard";
+import Button from "react-bootstrap/Button";
+
 function Cart() {
-  return <p>Soy la página del carrito</p>;
+  const { cart, cleanCart, totalPrice } = useContext(CartContext);
+
+  return (
+    <section className="container mt-4">
+      {cart.length > 0 ? (
+        <>
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+            {cart.map((book) => (
+              <div className="col d-flex align-items-stretch" key={book.id}>
+                <RenderCartCard book={book} />
+              </div>
+            ))}
+          </div>
+          <div className="text-center mt-4">
+            <p>Precio total: uS${Number(totalPrice.toFixed(2))}</p>
+            <Button variant="danger" onClick={cleanCart}>
+              Vaciar Carrito
+            </Button>
+          </div>
+        </>
+      ) : (
+        <p className="text-center">El carrito está vacío.</p>
+      )}
+    </section>
+  );
 }
+
 export default Cart;
