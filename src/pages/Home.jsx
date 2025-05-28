65f3dbf99c239da541fcfd25d8204f0dadb4bbeb
agusbattista <130807422+agusbattista@users.noreply.github.com>
2025-05-28 20:07:14 -0300

    corregi un bug que hacia que si hay un unico libro en el home no se visualizaba correctamente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import LoadingAnimation from "../components/LoadingAnimation";
 
 function Home({ books, loading, error }) {
   return (
-    <section className="container mt-4 d-flex justify-content-center">
+    <section className="container mt-4">
       {loading && <LoadingAnimation />}
       {error && !loading && <p>{error}</p>}
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
